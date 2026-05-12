e2e13e2135beffdcdbb664a1ca72203d50cd6a3a
TinsHuynh <tinhuynh06122005@gmail.com>
2026-05-12 13:57:19 +0700

    chore: remove project  file

--- scratch/test_dns.js
+++
@@ -1,8 +0,0 @@
-const dns = require('dns');
-dns.resolveSrv('_mongodb._tcp.quanlysinhvien.4wxqpuq.mongodb.net', (err, addresses) => {
-  if (err) {
-    console.error('DNS Resolve Error:', err);
-  } else {
-    console.log('DNS Resolve Success:', addresses);
-  }
-});
